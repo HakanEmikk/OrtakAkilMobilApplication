9039ed8bc34d050de393a9ea5eb35a6b600f6f15
hakanemik51 <hkn.emk51@gmail.com>
2025-09-29 13:54:35 +0300

    A viewmodel was created for the register and a function was added.

--- app/src/main/java/com/hakanemik/ortakakil/viewmodel/RegisterPageViewModel.kt
+++ app/src/main/java/com/hakanemik/ortakakil/viewmodel/RegisterPageViewModel.kt
@@ -1,6 +1,24 @@
 package com.hakanemik.ortakakil.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hakanemik.ortakakil.entity.RegisterApiResponse
+import com.hakanemik.ortakakil.entity.RegisterRequest
+import com.hakanemik.ortakakil.entity.Resource
+import com.hakanemik.ortakakil.repo.OrtakAkilDaoRepository
+import kotlinx.coroutines.launch
 
 class RegisterPageViewModel :ViewModel() {
+    var OrtakAkilDaoRepository = OrtakAkilDaoRepository()
+    private val _uiState = MutableLiveData<Resource<RegisterApiResponse>>()
+    val uiState: LiveData<Resource<RegisterApiResponse>> = _uiState
+    fun register(registerRequest: RegisterRequest){
+        viewModelScope.launch {
+            _uiState.value = Resource.Loading
+            _uiState.value = OrtakAkilDaoRepository.register(registerRequest)
+
+        }
+    }
 }

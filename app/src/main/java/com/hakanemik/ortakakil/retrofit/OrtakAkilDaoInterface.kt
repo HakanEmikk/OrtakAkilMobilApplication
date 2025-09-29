bdaf2367463b19961bd92a8aba75d97d9a7533b8
hakanemik51 <hkn.emk51@gmail.com>
2025-09-29 13:50:34 +0300

    A post has been added to register for dao.

--- app/src/main/java/com/hakanemik/ortakakil/retrofit/OrtakAkilDaoInterface.kt
+++ app/src/main/java/com/hakanemik/ortakakil/retrofit/OrtakAkilDaoInterface.kt
@@ -2,6 +2,8 @@ package com.hakanemik.ortakakil.retrofit
 
 import com.hakanemik.ortakakil.entity.LoginApiResponse
 import com.hakanemik.ortakakil.entity.LoginRequest
+import com.hakanemik.ortakakil.entity.RegisterApiResponse
+import com.hakanemik.ortakakil.entity.RegisterRequest
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.FormUrlEncoded
@@ -10,4 +12,6 @@ import retrofit2.http.POST
 interface OrtakAkilDaoInterface {
     @POST("/api/Auth/login")
     suspend  fun login(@Body loginRequest: LoginRequest): LoginApiResponse
+    @POST("/api/Auth/register")
+    suspend fun register(@Body registerRequest: RegisterRequest): RegisterApiResponse
 }

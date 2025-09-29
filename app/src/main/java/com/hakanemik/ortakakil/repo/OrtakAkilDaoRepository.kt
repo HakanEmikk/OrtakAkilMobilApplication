21181c9da1ec29c36488016f696140e2d3ef7e9a
hakanemik51 <hkn.emk51@gmail.com>
2025-09-29 13:52:49 +0300

    A function was created for registration and thenecessary checks were performed.

--- app/src/main/java/com/hakanemik/ortakakil/repo/OrtakAkilDaoRepository.kt
+++ app/src/main/java/com/hakanemik/ortakakil/repo/OrtakAkilDaoRepository.kt
@@ -5,6 +5,8 @@ import com.google.gson.Gson
 import com.hakanemik.ortakakil.entity.ErrorResponse
 import com.hakanemik.ortakakil.entity.LoginApiResponse
 import com.hakanemik.ortakakil.entity.LoginRequest
+import com.hakanemik.ortakakil.entity.RegisterApiResponse
+import com.hakanemik.ortakakil.entity.RegisterRequest
 import com.hakanemik.ortakakil.entity.Resource
 import com.hakanemik.ortakakil.retrofit.ApiUtils
 import com.hakanemik.ortakakil.retrofit.OrtakAkilDaoInterface
@@ -29,8 +31,6 @@ class OrtakAkilDaoRepository {
             null
         }
        val userFriendlyMessage = when (backendMessage) {
-           "Password is required" -> "Lütfen şifrenizi girin"
-           "Email is required" -> "Lütfen e-posta adresinizi girin"
            "Email or password is incorrect." -> "E-posta veya şifre hatalı"
            else -> "Bir hata oluştu, lütfen tekrar deneyin"
        }
@@ -42,6 +42,29 @@ class OrtakAkilDaoRepository {
        Resource.Error("Beklenmeyen hata: ${e.localizedMessage}")
     }
   }
+    suspend fun register(registerRequest: RegisterRequest):Resource<RegisterApiResponse>{
+        return try {
+            val response=ortakAkilDaoInterface.register(registerRequest)
+            Resource.Success(response)
+        }catch (e: retrofit2.HttpException){
+            val errorBody=e.response()?.errorBody()?.string()
+            val backendMessage= try {
+                Gson().fromJson(errorBody,ErrorResponse::class.java).message
+            }catch (ex:Exception){
+                null
+            }
+            val userFriendlyMessage = when (backendMessage) {
+                "This email address is already in use." -> "Bu e-posta adresi zaten kullanılıyor."
+                else -> "Bir hata oluştu, lütfen tekrar deneyin"
+            }
+            Resource.Error(userFriendlyMessage, e.code())
+        } catch (e: java.net.UnknownHostException) {
+            Resource.Error("İnternet bağlantınızı kontrol edin")
+        }
+        catch (e:Exception){
+            Resource.Error("Beklenmeyen hata: ${e.localizedMessage}")
+        }
+    }
 
 
 }

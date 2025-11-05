e8292973ef96c1af8d95355fb8d01cc7e5a01113
hakanemik51 <hkn.emk51@gmail.com>
2025-11-05 13:32:43 +0300

    An update has been made for orientation and page transition.

--- app/src/main/java/com/hakanemik/ortakakil/MainActivity.kt
+++ app/src/main/java/com/hakanemik/ortakakil/MainActivity.kt
@@ -1,36 +1,37 @@
 package com.hakanemik.ortakakil
 
-import LoginPage
+
 import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.google.gson.Gson
+import com.hakanemik.ortakakil.ui.page.AnswerPage
 import com.hakanemik.ortakakil.ui.page.HomePage
-
+import com.hakanemik.ortakakil.ui.page.LoginPage
 import com.hakanemik.ortakakil.ui.page.RegisterPage
 import com.hakanemik.ortakakil.ui.theme.OrtakAkilTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lockOrientationForPhones()
 
         setContent {
             OrtakAkilTheme {
@@ -47,20 +48,39 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+    private fun lockOrientationForPhones() {
+        val configuration = resources.configuration
+        val screenWidthDp = configuration.screenWidthDp
+
+        // 600dp altı telefonlar için portrait kilitle
+        if (screenWidthDp < 600) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        } else {
+            // Tablet ve üstü - serbest bırak
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        }
+    }
 }
 
 @Composable
 fun PageSelect(navController: NavHostController, snackbarHostState: SnackbarHostState) {
 
     NavHost(navController=navController,startDestination = "login_page"){
-        composable("login_page"){
+        composable(route = "login_page"){
             LoginPage(navController,snackbarHostState)
         }
-        composable("register_page"){
+        composable(route = "register_page"){
             RegisterPage(navController,snackbarHostState)
         }
-        composable("home_page"){
+        composable(route="home_page"){
             HomePage(navController,snackbarHostState)
+        }
+        composable(route = "answer_page/{question}",
+            arguments = listOf(navArgument("question"){type = NavType.StringType})
+            ) {backStackEntry ->
+            val question = backStackEntry.arguments?.getString("question")
+            AnswerPage(navController,snackbarHostState,question)
+
         }
         /*composable("details_page/{food}", arguments = listOf(
             navArgument("food"){
